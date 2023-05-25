685c9b36dc142cbf7c7f0bb599dc9c1e3c117c0e
Guy Yaffe Ermoza <guyyaffear@gmail.com>
2023-05-25 14:30:23 +0300

    change mockttp into localhost

--- test/integration/smoke-test.spec.ts
+++ test/integration/smoke-test.spec.ts
@@ -1,66 +1,70 @@
-import * as path from 'path';
-import { spawn, ChildProcess } from 'child_process';
-import * as StaticServer from 'static-server';
-import * as puppeteer from 'puppeteer';
+import * as path from "path";
+import { spawn, ChildProcess } from "child_process";
+import * as StaticServer from "static-server";
+import * as puppeteer from "puppeteer";
 
-import { expect } from '../test-setup';
+import { expect } from "../test-setup";
 
 function startWebServer() {
-    return new Promise((resolve) => {
-        new StaticServer({
-            port: 7654,
-            rootPath: path.join(__dirname, '..', '..', 'dist')
-        }).start(resolve)
-    });
+  return new Promise((resolve) => {
+    new StaticServer({
+      port: 7654,
+      rootPath: path.join(__dirname, "..", "..", "dist"),
+    }).start(resolve);
+  });
 }
 
 async function startServer() {
-    process.env.OCLIF_TS_NODE = '0';
-    const serverPath = path.join('.httptoolkit-server', 'httptoolkit-server', 'bin', 'run');
-    const serverProcess = spawn(serverPath, ['start'], {
-        stdio: 'inherit'
-    });
-    serverProcess.on('error', (error) => console.error('Server start failed', error));
-    serverProcess.on('exit', (code, signal) => {
-        console.log(`Server exited with ${code ?? signal}`);
-    });
+  process.env.OCLIF_TS_NODE = "0";
+  const serverPath = path.join(
+    ".httptoolkit-server",
+    "httptoolkit-server",
+    "bin",
+    "run"
+  );
+  const serverProcess = spawn(serverPath, ["start"], {
+    stdio: "inherit",
+  });
+  serverProcess.on("error", (error) =>
+    console.error("Server start failed", error)
+  );
+  serverProcess.on("exit", (code, signal) => {
+    console.log(`Server exited with ${code ?? signal}`);
+  });
 
-    return serverProcess;
+  return serverProcess;
 }
 
-describe('Smoke test', function () {
-    this.timeout(10000);
+describe("Smoke test", function () {
+  this.timeout(10000);
 
-    let browser: puppeteer.Browser;
-    let server: ChildProcess;
+  let browser: puppeteer.Browser;
+  let server: ChildProcess;
 
-    beforeEach(async () => {
-        [ browser, server ] = await Promise.all([
-            puppeteer.launch({
-                headless: true,
-                slowMo: 0,
-                timeout: 10000,
-                args: ['--no-sandbox']
-            }),
-            startServer(),
-            startWebServer()
-        ]);
-    });
+  beforeEach(async () => {
+    [browser, server] = await Promise.all([
+      puppeteer.launch({
+        headless: true,
+        slowMo: 0,
+        timeout: 10000,
+        args: ["--no-sandbox"],
+      }),
+      startServer(),
+      startWebServer(),
+    ]);
+  });
 
-    afterEach(async () => {
-        await Promise.all([
-            browser.close(),
-            server.kill()
-        ]);
-    });
+  afterEach(async () => {
+    await Promise.all([browser.close(), server.kill()]);
+  });
 
-    it('can load the app', async () => {
-        const page = await browser.newPage();
-        await page.goto('http://local.httptoolkit.tech:7654');
+  it("can load the app", async () => {
+    const page = await browser.newPage();
+    await page.goto("http://pipe.tech:7654");
 
-        await page.waitFor('h1');
-        const heading = await page.$eval('h1', (h1) => h1.innerHTML);
+    await page.waitFor("h1");
+    const heading = await page.$eval("h1", (h1) => h1.innerHTML);
 
-        expect(heading).to.equal('Intercept HTTP');
-    });
+    expect(heading).to.equal("Intercept HTTP");
+  });
 });
